6808f6f3db0b416e7f1df2774a87ad9a49de18e4
Monarc <francomatiaslujan23@gmail.com>
2021-10-25 12:31:48 -0300

    urls and views main modified

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.index, name="index"),
-    path("v1/", views.v1, name="view 1"),
-    path("fecha/", views.show_date, name="Fecha actual")
+    path("fecha/", views.show_date, name="Fecha actual"),
+    path("todo/<int:id>", views.to_do_list, name="ToDoList")
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 import datetime
 
 
@@ -8,8 +9,16 @@ def index(request):
     return HttpResponse("<html><body><h1>dale anda la puta madre jajaja</h1></body></html>")
 
 
-def v1(request):
-    return HttpResponse("view 1")
+def to_do_list(request, id):
+    """
+    here we GET the object by id in our ToDoList tab from
+    our db and display it's name using ls.name,
+    even though objects.get is not part of python, it will work
+    the same will happen with ls.name
+    """
+    ls = ToDoList.objects.get(id=id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, item.text))
 
 
 def show_date(request):
