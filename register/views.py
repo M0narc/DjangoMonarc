0f4e671ae2e5071484726eb185ef1f8d61b8616f
Monarc <francomatiaslujan23@gmail.com>
2021-12-15 13:54:04 -0300

    new value in settings, new url, new registerApp, forms, templates and views from registerApp

--- register/views.py
+++ register/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# since we do not want duplicate code we just import RegisterForm from forms.py
+from .forms import RegisterForm
 
 # Create your views here.
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        # once it's created it redirects to our home
+        return redirect("/home")
+    else:
+        form = RegisterForm()
+
+    return render(response, "register/register.html", {"form": form})
